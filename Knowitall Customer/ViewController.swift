91c251666476a5e482c017dc804ac19d004e01e3
Ramniwas Patidar <ramniwas.patidar@gmail.com>
2023-11-30 10:02:34 +0530

    Initial Commit

---
+++ Knowitall Customer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Knowitall Customer
+//
+//  Created by Ramniwas Patidar on 30/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
